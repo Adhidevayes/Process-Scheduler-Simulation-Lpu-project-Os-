ad981eef6a4e46871d3e5f72db44a4b6f252d955
Adhidevayes <135628221+Adhidevayes@users.noreply.github.com>
2026-05-12 10:39:57 +0530

    Add FCFS scheduling algorithm and Process class

--- scheduler_gui.py
+++ scheduler_gui.py
@@ -1,1 +1,39 @@
+import customtkinter as ctk
+from tkinter import ttk, messagebox
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import copy
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+
+
+# Process Class
+class Process:
+    def __init__(self, pid, at, bt):
+        self.pid = pid
+        self.arrival = at
+        self.burst = bt
+        self.remaining = bt
+        self.waiting = 0
+        self.turnaround = 0
+
+
+
+# Algorithms
+def fcfs(processes):
+    processes.sort(key=lambda x: x.arrival)
+    time = 0
+    gantt = []
+
+    for p in processes:
+        if time < p.arrival:
+            time = p.arrival
+        gantt.append((p.pid, time, time + p.burst))
+        time += p.burst
+        p.turnaround = time - p.arrival
+        p.waiting = p.turnaround - p.burst
+
+    return gantt, processes
 
